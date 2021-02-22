2e958a6ca0f55d7b008f3bf23cedaa69ae4966e8
erezlug <40339211+erezlug@users.noreply.github.com>
2021-02-22 09:53:03 +0200

    Update commands.js

--- src/const/commands.js
+++ src/const/commands.js
@@ -1,3 +1,3 @@
-var commands = [ "Activate", "Save me", "Help Me", "Stop" , "הצילו", "טרפי טרפי", "פרח אדום",   "זקוק לחילוץ"];
+var commands = [ "Activate", "Save me", "Help Me", "Stop" , "הצילו", "טרפי טרפי", "אורלי",   "זקוק לחילוץ"];
 
 export default commands;
